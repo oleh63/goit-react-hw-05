5d1aab50f4f2f791db250c094a18dc987d1e506e
Oleh Skrypets <olehskrupets@gmail.com>
2025-01-14 17:44:35 +0200

    add react.lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import { Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
-import HomePage from "./pages/HomePage/HomePage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
-import MoviesPage from "./pages/MoviesPage/MoviesPage";
-import MovieCast from "./components/MovieCast/MovieCast";
-import MovieReviews from "./components/MovieReviews/MovieReviews";
+
+const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = React.lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = React.lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = React.lazy(() =>
+  import("./pages/NotFoundPage/NotFoundPage")
+);
+const MovieCast = React.lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieReviews = React.lazy(() =>
+  import("./components/MovieReviews/MovieReviews")
+);
 
 function App() {
   return (
     <div>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";
 import styles from "./MovieList.module.css";
+import { Link, useLocation } from "react-router-dom";
 
 const MovieList = ({ movies }) => {
+  const location = useLocation();
   return (
     <ul className={styles.list}>
       {movies.map((movie) => (
         <li key={movie.id} className={styles.item}>
-          <Link to={`/${movie.id}`} className={styles.link}>
+          <Link
+            to={`/${movie.id}`}
+            state={{ from: location }}
+            className={styles.link}
+          >
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
